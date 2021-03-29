07d9de33470518c8dd5ac6b0ab65d94abd5a14b7
Igor Rosberg <igorosbergster@gmail.com>
2021-03-29 10:59:31 -0300

    adicionando rotas de adição e remoção dep rodutos

--- servidor/index.js
+++ servidor/index.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 3031;
 
-const produtos = [
+app.use(express.json());
+
+var produtos = [
   {
-    id: '1',
+    id: uuidv4(),
     produto: 'Bola',
     descricao: 'Bola média',
     img: 'https://imagepng.org/wp-content/uploads/2017/10/bola.png',
     botao: 'Comprar bola',
   },
   {
-    id: '2',
+    id: uuidv4(),
     produto: 'Chuteira',
     descricao: 'Chuteira preta',
     img: 'https://rogerstenis.vteximg.com.br/arquivos/ids/2574818-505-505/1000414281_1.jpg?v=636881799375300000',
     botao: 'Comprar chuteira',
   },
   {
-    id: '3',
+    id: uuidv4(),
     produto: 'Caneleira',
     descricao: 'Caneleira amarela',
     img: 'https://images.tcdn.com.br/img/img_prod/628041/caneleira_alyki_poker_adulto_10689_1_20200727161314.png',
@@ -37,6 +40,18 @@ app.get('/produtos', (req, res) => {
 app.get('/produtos/:id', (req, res) => {
   const produto = produtos.filter((produto) => produto.id == req.params.id);
   res.send(produto[0]);
+});
+
+app.post('/produtos', (req, res) => {
+  const novoProduto = req.body;
+  novoProduto.id = uuidv4();
+  produtos.push(novoProduto);
+  res.send(novoProduto);
+});
+
+app.delete('/produtos/:id', (req, res) => {
+  produtos = produtos.filter((produto) => produto.id != req.params.id)
+  res.send(produtos);
 });
 
 app.listen(port, '0.0.0.0', () => {
